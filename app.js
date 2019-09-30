1344c6ae2552d069943d8437dfbe107be75a2fbc
Finn <finnterdal@gmail.com>
2019-09-30 13:33:22 -0500

    finally got that key prop working

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 const { React, ReactDOM } = window
 const ce = React.createElement
 
-const names = ['Finn', 'Collin', 'Priti']
+const instructors = [
+  { id: 1, name: 'Finn' },
+  { id: 2, name: 'Collin' },
+  { id: 3, name: 'Priti' },
+]
 
 const ListItem = props => {
-  const { name } = props
-  return ce('li', {}, name)
+  const { name, key } = props
+  return ce('li', { key: key }, name)
 }
-const ul = ce('ul', {}, names.map(name => ListItem({ name })))
+const ul = ce(
+  'ul',
+  null,
+  instructors.map(instructor =>
+    ListItem({ name: instructor.name, key: instructor.id })
+  )
+)
 
-const h2 = ce('h2', {}, 'Hello World!')
-const div = ce('div', {}, h2, ul)
+class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { counter: 0 }
+  }
+  handleChange() {
+    console.log('CHANGING COUNTER')
+    this.setState({ counter: 1000 })
+  }
+  render() {
+    return ce(
+      'div',
+      null,
+      ce('p', null, `Counter: ${this.state.counter}`),
+      ce('button', { onChange: this.handleChange }, `Set Counter to 1000`)
+    )
+  }
+}
+
+const h2 = ce('h2', null, 'Hello World!')
+const div = ce('div', null, h2, ul, ce(Counter))
 
 const app = document.getElementById('app')
 ReactDOM.render(div, app)
