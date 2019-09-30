4bf683254acb697d5e386ce0170519f351c38c03
Finn <finnterdal@gmail.com>
2019-09-30 13:00:46 -0500

    create first functional component

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const { React, ReactDOM } = window
+const ce = React.createElement
 
-const h2 = React.createElement('h2', {}, 'Hello World!')
+const names = ['Finn', 'Collin', 'Priti']
 
-const appContainer = document.getElementById('app')
-ReactDOM.render(h2, appContainer)
+const ListItem = props => {
+  const { name } = props
+  return ce('li', {}, name)
+}
+const ul = ce('ul', {}, names.map(name => ListItem({ name })))
+
+const h2 = ce('h2', {}, 'Hello World!')
+const div = ce('div', {}, h2, ul)
+
+const app = document.getElementById('app')
+ReactDOM.render(div, app)
