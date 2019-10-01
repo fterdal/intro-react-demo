f12202d4e145b37805e72bd51c4b7adffbd8381e
Finn <finnterdal@gmail.com>
2019-10-01 08:57:53 -0500

    simple JSX example

--- app.js
+++ app.js
@@ -1,51 +1,10 @@
 /* eslint-disable function-paren-newline */
 const { React, ReactDOM } = window
-const ce = React.createElement
-
-const dummyInstructors = [
-  { id: 1, name: 'Finn' },
-  { id: 2, name: 'Collin' },
-  { id: 3, name: 'Priti' },
-]
-
-// Class React Component
-class Instructor extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      checked: false,
-    }
-  }
-  render() {
-    const checkedStatus = this.state.checked ? 'checked' : 'unchecked'
-    const { name } = this.props
-    return ce(
-      'div',
-      {
-        className: ['instructor', checkedStatus].join(' '),
-        onClick: () => this.setState({ checked: true }),
-      },
-      ce('p', null, name),
-      ce('img', { src: `https://robohash.org/${name}?size=200x200` })
-    )
-  }
-}
-
-// Functional React Component
-const InstructorList = props => {
-  const { instructors } = props
-  return ce(
-    'div',
-    { className: 'instructor-list' },
-    instructors.map(instructor =>
-      ce(Instructor, { name: instructor.name, key: instructor.id })
-    )
-  )
-}
 
 const app = document.getElementById('app')
+const Thing = () => <h1>Hello World!</h1>
 
 ReactDOM.render(
-  ce('div', null, ce(InstructorList, { instructors: dummyInstructors })),
+  <Thing />,
   app
 )
